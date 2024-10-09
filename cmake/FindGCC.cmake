e36d4fc2d8f808df01bc271e166595813a78b221
stefphd <stefano.lovato.1@phd.unipd.it>
2024-10-09 11:23:33 +0200

    Change in Cmake project

--- cmake/FindGCC.cmake
+++ cmake/FindGCC.cmake
@@ -7,13 +7,18 @@
 # GCC_DIR           - Root directory of GCC
 # GCC_EXECUTABLE    - GCC executable
 
+# Test the GCCDIR environment variable and create cmake GCC_DIR variable accordingly
+set(GCC_DIR $ENV{GCCDIR})
+
 # Find executable
 find_program(GCC_EXECUTABLE
     NAMES bin/gcc.exe
-    PATHS $ENV{GCCDIR})
+    PATHS ${GCC_DIR})
+
 # Extract root directory
 find_path(GCC_DIR
     NAMES bin/gcc.exe)
+
 # Handle GCC_FOUND
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(GCC DEFAULT_MSG GCC_EXECUTABLE GCC_DIR)
